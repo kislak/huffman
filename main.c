a4b1ac0097b6e5e79d16a485eca37330d6168b66
serge <kislak7@gmail.com>
2015-04-19 22:15:32 +0300

    Added tree serialization.

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 int statistics[256] = {0};
 int order[256] = {0};
 int map[256] = {0};
+int size = 0;
 
 typedef struct node {
     int item, stat;
@@ -17,24 +18,26 @@ int comparator(const void *a, const void *b) {
 
 node * combine(node *first, node *second);
 void build_map(node *head, int i, int n);
-void create_archive(node *head, char *ofile);
+void create_archive(char *fname);
 node * extract_tree(char *file);
 
 int main(int argc, char** argv) {
     int i;
-    int size = 0;
-    
+    node *head = NULL;
+
     if (!strcmp(argv[2], "-c")) {
         get_statistics(statistics, argv[1]);
-        
+//        TODO
+//        statistics = get_statistics(argv[1])
+
+//        build tree
         for (i = 0; i < 255; i++) { order[i] = i; };
-        
         qsort(order, 256, sizeof (int), comparator);
 
         while (statistics[order[size]] && size < 256) { size++; }
+
         printf("size %d\n", size);
-        
-        node *head = NULL;
+        if (size == 0) { return 0; };
         
         for (i = 0; i < size; i++) {
             node *n = malloc(sizeof(node));
@@ -46,30 +49,31 @@ int main(int argc, char** argv) {
             head = n;
         };
            
-        while (head -> next) { head = combine(head, head -> next); }        
-        build_map(head, 1, 1);
+        while (head -> next) { head = combine(head, head -> next); }
         
-        create_archive(head, argv[3]);
+//        build map
+        build_map(head, 0, 0);
         
-//        for (i = 0; i < 256; i++)
-//        {
-//            if (map[i]){
-//                printf("%d - %d - %d\n", i, map[i], statistics[i]);  
-//            };
-//        }        
+        create_archive(argv[3]);
+        
+        // check
+        for (i = 0; i < 256; i++)
+        {
+            if (map[i]){
+                printf("%d - %d - %d\n", i, map[i], statistics[i]);
+            };
+        }
         return (EXIT_SUCCESS);
     };
 
 
     if (!strcmp(argv[2], "-x")) {
-        node *head = NULL;
         head = extract_tree(argv[1]);
-        build_map(head, 1, 1);
         
         printf("extracted\n");
         return (EXIT_SUCCESS);
     };
-} 
+}
 
 node * combine(node *first, node *second){
     node *n = NULL, *head = NULL;
@@ -100,22 +104,53 @@ node * combine(node *first, node *second){
     return head;
 }
 
-void build_map(node *head, int i, int n){
+void build_map(node *head, int code, int depth){
     if (head -> item > 0) {
-        map[head->item] = i;
-        statistics[head->item] = n;
+        map[head->item] = code;
+        statistics[head->item] = depth;
     }    
-    if (head -> left) { build_map(head -> left, (i << 1) + 1, n+1); };
-    if (head -> right) { build_map(head -> right, (i << 1), n+1); };
+    if (head -> left) { build_map(head -> left, (code << 1) + 1, depth+1); };
+    if (head -> right) { build_map(head -> right, (code << 1), depth+1); };
 }
 
-void create_archive(node *head, char *ofile){
-    printf(ofile);
-//    archive_tree();
-//    archive_data();
+void create_archive(char *fname){
+   FILE *pFile = fopen(fname, "wb");
+   
+   int i;
+   
+   for (i = 0; i < 256; i++) {
+        fputc(map[i], pFile);
+        fputc(statistics[i], pFile);
+   };
+   
+   fclose(pFile);
 };
 
-node * extract_tree(char *file){
+//void archive_tree(){
+//
+//}
+    
+node * extract_tree(char *fname){
+    int i, c;
+    FILE *pFile = fopen(fname, "rb");
+    if (pFile == NULL) perror("Error opening file");
+    else {
+           for (i = 0; i < 256; i++) {
+             c = fgetc(pFile);
+             map[i] = c;
+             c = fgetc(pFile);
+             statistics[i] = c;
+           };
+           fclose(pFile);
+    };
+    
+    // check
+    for (i = 0; i < 256; i++)
+    {
+        if (map[i]){
+            printf("%d - %d - %d\n", i, map[i], statistics[i]);
+        };
+    }
     NULL;
 }
         
--- readme.txt
+++ readme.txt
@@ -15,3 +15,16 @@ extract:
 huff out -x input2.txt
 
 ? get random data from dd
+
+File.open('five.txt', 'w') do |file|
+  20.times do |i|
+    ((2**i-1)+1).times do
+      12.times do |j|
+        file.putc i+ 20 * j
+       end
+    end
+  end
+  (20..255).each do |i|
+      file.putc i
+  end
+end
